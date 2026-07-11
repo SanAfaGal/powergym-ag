314e9558829bb1d552ae0e00c7f9e34af1c7b56d
Santiago Afanador <afanadorgaleano@gmail.com>
2026-07-10 20:55:15 -0500

    test(db): add coverage for gender_type_enum and duration_type_enum

--- supabase/tests/database/001_enums.test.sql
+++ supabase/tests/database/001_enums.test.sql
@@ -1,13 +1,11 @@
 -- supabase/tests/database/001_enums.test.sql
 begin;
-select plan(7);
+select plan(9);
 
 select has_extension('pgcrypto');
 select has_extension('pg_trgm');
 select has_extension('pg_cron');
 
-select enum_range(null::app_role) = '{admin,employee}'::app_role[] as ok
-  from (select 1) t;
 select ok(
   (select enum_range(null::app_role)::text = '{admin,employee}'),
   'app_role has expected values'
@@ -15,6 +13,14 @@ select ok(
 select ok(
   (select enum_range(null::document_type_enum)::text = '{CC,TI,CE,PP}'),
   'document_type_enum has expected values'
+);
+select ok(
+  (select enum_range(null::gender_type_enum)::text = '{male,female,other}'),
+  'gender_type_enum has expected values'
+);
+select ok(
+  (select enum_range(null::duration_type_enum)::text = '{day,week,month,year}'),
+  'duration_type_enum has expected values'
 );
 select ok(
   (select enum_range(null::subscription_status_enum)::text = '{active,expired,pending_payment,scheduled,canceled}'),
